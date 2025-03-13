03c44a487ebe8c80ffee37ab3d14550523ebcf5a
mikew123 <mikew123@gmail.com>
2025-03-13 12:01:02 -0500

    removed build, install and log folders

--- build/robo24_stuff/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/mike/roborama25_ws/install/robo24_stuff'
--- install/robo24_interfaces/local/lib/python3.10/dist-packages/robo24_interfaces/srv/__init__.py
+++
@@ -1,1 +0,0 @@
-from robo24_interfaces.srv._claw import Claw  # noqa: F401
